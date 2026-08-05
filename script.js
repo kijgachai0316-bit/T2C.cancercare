7ebeeef79a3ea2c7aa91d3bf4df10d6653e2c1da
kijgachai0316-bit <kijgachai0316@gmail.com>
2026-08-05 20:30:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 console.log("T2C Cancer Care");
+
+/* =========================================
+   T2C MOBILE MENU
+========================================= */
+
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.querySelector(".navbar ul");
+
+if (menuToggle && navMenu) {
+
+    menuToggle.addEventListener("click", function () {
+
+        navMenu.classList.toggle("active");
+
+        const icon = menuToggle.querySelector("i");
+
+        if (navMenu.classList.contains("active")) {
+            icon.classList.remove("fa-bars");
+            icon.classList.add("fa-xmark");
+        } else {
+            icon.classList.remove("fa-xmark");
+            icon.classList.add("fa-bars");
+        }
+
+    });
+
+
+    /* กดเมนูแล้วให้เมนูปิดอัตโนมัติ */
+
+    navMenu.querySelectorAll("a").forEach(function (link) {
+
+        link.addEventListener("click", function () {
+
+            navMenu.classList.remove("active");
+
+            const icon = menuToggle.querySelector("i");
+
+            icon.classList.remove("fa-xmark");
+            icon.classList.add("fa-bars");
+
+        });
+
+    });
+
+}
